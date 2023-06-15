ee546d448e3038ebea69ac0c6e6603dd25f16c6f
unknown <vins3016@gmail.com>
2023-06-15 20:28:58 +0700

    adding javascript

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-function texting(){
+function textingup(){   
     var text = document.getElementById("text");
     var btn1 = document.getElementById("btn^");
-    
+    document.getElementById("text").innerHTML = "UP";
+}
+
+function textingleft(){
+    var text = document.getElementById("text");
+    var btn1 = document.getElementById("btn^");
+    document.getElementById("text").innerHTML = "LEFT";
+}
+
+function textingright(){
+    var text = document.getElementById("text");
+    var btn1 = document.getElementById("btn^");
+    document.getElementById("text").innerHTML = "RIGHT";
+}
+
+function textingdown(){
+    var text = document.getElementById("text");
+    var btn1 = document.getElementById("btn^");
+    document.getElementById("text").innerHTML = "DOWN";
+}
+
+function textinga(){
+    var text = document.getElementById("text");
+    var btn1 = document.getElementById("btn^");
+    document.getElementById("text").innerHTML = "A";
+}
+
+function textingb(){
+    var text = document.getElementById("text");
+    var btn1 = document.getElementById("btn^");
+    document.getElementById("text").innerHTML = "B";
 }
